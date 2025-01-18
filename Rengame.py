990d7fd9fc3cf834a5ae688835737af166591eef
Jananporn Banjob <jananporn.banjob.gamil.com>
2025-01-18 21:20:05 +0700

    change Rengame and pageone.kv

--- Rengame.py
+++ Rengame.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 
+# from kivy.uix.button import Button
+# from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 
 from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.resources import resource_add_path
@@ -7,8 +11,17 @@ from kivy.resources import resource_add_path
 resource_add_path('D:\\New folder (2)\debug-font')
 LabelBase.register(DEFAULT_FONT, 'DebugF.otf')
 
-class PageoneApp(App) :
+class ScreenOne(Screen):
+    def change_screen(self):
+        self.manager.current = 'screen_two'
+
+class ScreenTwo(Screen):
     pass
+
+
+class PageoneApp(App) :
+    def build(self):
+        return Builder.load_file('pageone.kv')
 
 if __name__ =='__main__':
     PageoneApp().run()
